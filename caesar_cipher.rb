36addb51d300d2d96a8f902e7d130ee18c3f6b63
Daniel Florczak <danyflorczak@gmail.com>
2023-05-10 16:11:48 +0200

    add: decrypt the message method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -11,4 +11,17 @@ class CaesarCipher
     end
     encrypted_message
   end
+
+  def decrypt_message(message, shift, decrypted_message="")
+    message.each_char do |char|
+      base = char.ord < 91 ? 65 : 97
+      if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+        rotation = (((char.ord - base) - shift) % 26) + base
+        decrypted_message += rotation.chr
+      else
+        decrypted_message += char
+      end
+    end
+    decrypted_message
+  end
 end
